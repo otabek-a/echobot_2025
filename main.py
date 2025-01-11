985fbc71ee8ff093d43c14eb42e485310cdd339a
MacOS m3 <uzbzarif@gmail.com>
2025-01-11 17:06:07 +0500

    get me

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import requests
-print(requests.__version__)
+
+TOKEN ='1602686596:AAHdqoVLsAlXlFYDlihaQy1x_Tmilg7fqiM'
+
+URL = f"https://api.telegram.org/bot{TOKEN}"
+
+# Get me
+r = requests.get(URL + "/getMe")
+print(r.json())
