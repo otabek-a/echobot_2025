6460dde90824f3d55b41b5f2edcef1096e48f195
MacOS m3 <uzbzarif@gmail.com>
2025-01-11 17:12:15 +0500

    send message

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
 import os
 TOKEN = os.getenv("TOKEN")
-print(f'TOKEN: {TOKEN}')
+
+
+
+def get_updates():
+
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    r = requests.get(url)
+    return r.json()
+
+def send_message(text, chat_id):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
+    r=requests.get(url)
+    return r.json()
+
+CHAT_ID = "86775091"
+send_message("SALOM", CHAT_ID)
 
